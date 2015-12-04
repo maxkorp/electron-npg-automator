661c4724209d2a820f4e3e2fc88e4d4d4688c4ba
joshaber <joshaber@gmail.com>
2015-12-04 09:38:14 -0500

    Get the path for node-pre-gyp.

--- _ci/deploy.js
+++ _ci/deploy.js
@@ -1,13 +1,15 @@
 const cp = require('child_process');
 const fs = require('fs');
+const path = require('path');
 const modulePath = require('../util/module-path');
 const getTagInfo = require('../util/get-tag-info');
 
 getTagInfo()
   .then(({electronVersion}) => {
-    var cmd = 'node-pre-gyp';
+    var modulePath_ = modulePath();
+    var cmd = path.join(modulePath_, 'node_modules', '.bin', 'node-pre-gyp');
     var args = ['package', 'publish', '--runtime=electron', `--target=${electronVersion}`];
-    var proc = cp.spawn(cmd, args, {cwd: modulePath()});
+    var proc = cp.spawn(cmd, args, {cwd: modulePath_});
     proc.stdout.pipe(process.stdout);
     proc.stderr.pipe(process.stderr);
     proc.on('close', (code) => {
