ddb9446b2dadaaedfa960b3262fc082861b8f48c
joshaber <joshaber@gmail.com>
2015-12-04 17:42:23 -0500

    Use a custom prepare.

---
+++ _ci/custom-prepare.js
@@ -0,0 +1,21 @@
+var cp = require('child_process');
+​
+module.exports = function(cwd) {
+  return new Promise(function(resolve, reject) {
+    cp.spawn('npm', ['install', '--ignore-scripts'], {cwd: cwd})
+      .on('close', function(code) {
+        if (code != 0) {
+          return reject(code);
+        }
+        cp.spawn('node', ['lifecycleScripts/prepareForBuild.js'], {cwd: cwd})
+          .on('close', function(code) {
+            console.log('woah!');
+            console.log(code);
+            if (code != 0) {
+              return reject(code);
+            }
+            resolve();
+          });
+      });
+  });
+}
