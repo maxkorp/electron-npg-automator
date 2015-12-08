11bc54cf345c29434ee4292eb4baf47b8deecfa9
John Haley <johnh@axosoft.com>
2015-12-08 12:56:11 -0700

    Add option for a custom build script

--- _ci/build.js
+++ _ci/build.js
@@ -1,21 +1,26 @@
 const cp = require('child_process');
 const getTagInfo = require('../util/get-tag-info');
+const modulePath = require('../util/module-path');
 const moduleParentPath = require('../util/module-parent-path');
 const path = require('path');
+const {installNodeHeaders, rebuildNativeModules} = require('electron-rebuild');
+
+let buildScript;
+
+try {
+  buildScript = require('./custom-build');
+}
+catch(e) {
+  console.log('using default build script');
+  buildScript = function defaultBuildScript(electronVersion, moduleParentPath) {
+    return installNodeHeaders(electronVersion)
+      .then(() => rebuildNativeModules(electronVersion, moduleParentPath));
+  }
+}
 
 getTagInfo()
   .then(({electronVersion}) => {
-    var cmd = 'node';
-    var args = [
-      path.resolve('node_modules', '.bin', 'electron-rebuild'),
-      '-v', electronVersion.replace('v', ''),
-      '-m', moduleParentPath()
-    ];
+    electronVersion = electronVersion.replace('v', '');
 
-    var proc = cp.spawn(cmd, args);
-    proc.stdout.pipe(process.stdout);
-    proc.stderr.pipe(process.stderr);
-    proc.on('close', (code) => {
-      process.exit(code);
-    });
+    return buildScript(electronVersion, moduleParentPath(), modulePath());
   });
